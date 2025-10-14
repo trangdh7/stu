938475f8666985a4b5459aefd7d80beca8e9749f
trangdh7 <trangdh176168@gmail.com>
2025-10-14 16:34:20 +0700

    Ignore .vs folder and fix permission issue

--- Webkho20241021/obj/Debug/net8.0/Webkho20241021.AssemblyInfo.cs
+++ Webkho20241021/obj/Debug/net8.0/Webkho20241021.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webkho20241021")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+732c911e13cb522dc8b5255113e9dbe9032fead0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webkho20241021")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webkho20241021")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
